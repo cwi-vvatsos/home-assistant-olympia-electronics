db86265d874d5c90d815bbfdbf815a6022932bd3
cwi-vvatsos <vvatsos@cwi.gr>
2019-11-06 01:02:21 +0200

    updated for latest HA

--- olympia_electronics.py
+++ olympia_electronics.py
@@ -7,11 +7,11 @@ import logging, json, requests, jwt, re, datetime
 import voluptuous as vol
 
 from homeassistant.components.climate import (
-    ATTR_CURRENT_TEMPERATURE, ATTR_FAN_MODE, ATTR_OPERATION_MODE,
-    ATTR_SWING_MODE, PLATFORM_SCHEMA, STATE_AUTO, STATE_COOL, STATE_DRY,
-    STATE_FAN_ONLY, STATE_HEAT, STATE_OFF, SUPPORT_FAN_MODE, STATE_IDLE,
-    SUPPORT_OPERATION_MODE, SUPPORT_ON_OFF, SUPPORT_TARGET_TEMPERATURE,
-    ClimateDevice)
+    ATTR_CURRENT_TEMPERATURE, ATTR_FAN_MODE, ATTR_HVAC_MODE,
+    PLATFORM_SCHEMA, ClimateDevice)
+from homeassistant.components.climate.const import (
+    HVAC_MODE_AUTO, HVAC_MODE_HEAT, HVAC_MODE_OFF, CURRENT_HVAC_IDLE, SUPPORT_TARGET_TEMPERATURE,
+    CURRENT_HVAC_COOL, CURRENT_HVAC_HEAT, CURRENT_HVAC_OFF)
 from homeassistant.const import (
     ATTR_TEMPERATURE, CONF_EMAIL, CONF_PASSWORD, CONF_TOKEN, TEMP_CELSIUS, PRECISION_HALVES,
     PRECISION_TENTHS, PRECISION_WHOLE)
@@ -37,15 +37,15 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
 })
 
 HA_STATE_TO_OLYMPIA = {
-    STATE_HEAT: 'on',
-    STATE_OFF: 'off',
-    STATE_IDLE: 'idle'
+    HVAC_MODE_HEAT: 'on',
+    HVAC_MODE_OFF: 'off',
+    HVAC_MODE_AUTO: 'idle'
 }
 
 OLYMPIA_TO_HA_STATE = {
-    'on': STATE_HEAT,
-    'off': STATE_OFF,
-    'idle': STATE_IDLE
+    'on': HVAC_MODE_HEAT,
+    'off': HVAC_MODE_OFF,
+    'idle': HVAC_MODE_AUTO
 }
 
 
@@ -71,7 +71,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
                       headers=_headers, data=_login_details)
 
     resp = r.json()
-    _LOGGER.info("Got details %s",json.dumps(resp))
+    _LOGGER.debug("Got details %s",json.dumps(resp))
 
     if 'non_field_errors' in resp:
         _LOGGER.warning("Error logging in to Olympia Electronics Service: %s",resp['non_field_errors'])
@@ -86,7 +86,7 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     r = requests.get('https://iot-api.olympia-electronics.gr/v1/thermostats/', headers=_headers)
 
     resp = r.json()
-    _LOGGER.info("Got thermostats %s",json.dumps(resp))
+    _LOGGER.debug("Got thermostats %s",json.dumps(resp))
     entities = []
 
     for device in resp['results']:
@@ -101,7 +101,7 @@ class OlympiaElectronicsThermostat(ClimateDevice):
     """Representation of a Olympia Electronics Thermostat."""
 
     def __init__(self, login_details, id, name, status, min_temp, max_temp, precision, auth_token):
-        self._support_flags = SUPPORT_TARGET_TEMPERATURE | SUPPORT_ON_OFF | SUPPORT_OPERATION_MODE
+        self._support_flags = SUPPORT_TARGET_TEMPERATURE
         self._name = name
         self._login_details = login_details
         self._id = id
@@ -119,6 +119,7 @@ class OlympiaElectronicsThermostat(ClimateDevice):
         else:
             self._precision = PRECISION_TENTHS
         self.setStatus(status)
+        self._current_operation = None
 
     def setStatus(self ,status):
         self._temperature = status['temperature']
@@ -131,13 +132,13 @@ class OlympiaElectronicsThermostat(ClimateDevice):
     @property
     def supported_features(self):
         """Return the list of supported features."""
-#        _LOGGER.info('Called supported_features')
+        """_LOGGER.debug('Called supported_features')"""
         return self._support_flags
 
     @property
     def should_poll(self):
         """Return the polling state."""
-#        _LOGGER.info('Called should_poll')
+        """_LOGGER.debug('Called should_poll')"""
         return True
 
     @property
@@ -146,38 +147,45 @@ class OlympiaElectronicsThermostat(ClimateDevice):
             decToken = jwt.decode(self._AUTH_TOKEN, verify=False)
             dt = datetime.datetime.utcnow()
             if decToken.get('exp')<dt.timestamp():
-                _LOGGER.info('JWT EXPIRED')
+                _LOGGER.warning('JWT EXPIRED')
                 return False
             else:
                 return True
 
         except jwt.exceptions.DecodeError as err:
-            _LOGGER.info('JWT is invalid (%s): %s',err ,self._AUTH_TOKEN)
+            _LOGGER.error('JWT is invalid (%s): %s',err ,self._AUTH_TOKEN)
             return False
 
     @property
     def name(self):
-#        _LOGGER.info('Called name')
+        """_LOGGER.debug('Called name')"""
         return self._name
 
     def update(self):
         """Requested update"""
-#        _LOGGER.info('Called update')
+        """_LOGGER.debug('Called update')"""
         if not self.isValidToken:
             self.updateToken()
             if not self.isValidToken:
-                _LOGGER.info('Failed to fetch updated token')
+                _LOGGER.warning('Failed to fetch updated token')
                 return
 
         _headers = {
             'Authorization'  : 'JWT {}'.format(self._AUTH_TOKEN)
         }
 
+        _LOGGER.debug('Getting update from the thermostat api')
         r = requests.get('https://iot-api.olympia-electronics.gr/v1/thermostats/{}/'.format(self._id), headers=_headers)
         if r.status_code != 200:
             _LOGGER.error('Could not update! Got %d status code from olympia-electronics',r.status_code)
+            if r.status_code == 401:
+                self.updateToken()
+                if not self.isValidToken:
+                    _LOGGER.warning('Failed to fetch updated token')
+                    return
         else:
             resp = r.json()
+            _LOGGER.debug('Updated info from thermostat: %s',resp)
             self.setStatus(resp['status'])
 
     def updateToken(self):
@@ -189,7 +197,7 @@ class OlympiaElectronicsThermostat(ClimateDevice):
                           headers=_headers, data=self._login_details)
 
         resp = r.json()
-        _LOGGER.info("Got details %s",json.dumps(resp))
+        _LOGGER.debug("Got details %s",json.dumps(resp))
 
         if 'non_field_errors' in resp:
             _LOGGER.warning("Error logging in to Olympia Electronics Service: %s",resp['non_field_errors'])
@@ -199,70 +207,92 @@ class OlympiaElectronicsThermostat(ClimateDevice):
 
     @property
     def temperature_unit(self):
-#        _LOGGER.info('Called temperature_unit')
+        """_LOGGER.debug('Called temperature_unit')"""
         return TEMP_CELSIUS
 
     @property
     def current_temperature(self):
         """Return the current temperature."""
-#        _LOGGER.info('Called current_temperature')
+        """_LOGGER.debug('Called current_temperature')"""
         return self._temperature
 
     @property
     def target_temperature(self):
         """Return the temperature we try to reach."""
-#        _LOGGER.info('Called target_temperature')
+        """_LOGGER.debug('Called target_temperature')"""
         return self._target_temperature
 
     @property
-    def current_operation(self):
+    def hvac_mode(self):
         """Return current operation ie. heat, cool, idle."""
-#        _LOGGER.info('Called current_operation')
+        _LOGGER.debug('requested hvac_mode: %s',self._is_on)
         if self._is_on:
             if self._burner_on:
-                return STATE_HEAT
+                return HVAC_MODE_HEAT
             else:
-                return STATE_IDLE
+                return HVAC_MODE_AUTO
         else:
-            return STATE_OFF
+            return HVAC_MODE_OFF
 
-    def set_operation_mode(self, operation_mode):
-        _LOGGER.info('Will set mode')
+    def set_hvac_mode(self, operation_mode):
+        _LOGGER.debug('Will set mode')
         state = HA_STATE_TO_OLYMPIA.get(operation_mode)
         if state is None:
             return
-        _LOGGER.info('Mode is not none')
+        _LOGGER.debug('Mode is not none')
         if state == 'on':
             self._target_temperature = self._temperature + 1
-            _LOGGER.info("Will set tempetature to {}".format(self._target_temperature))
+            _LOGGER.debug("Will set tempetature to {}".format(self._target_temperature))
             self._is_on = True
         elif state == 'off':
-            _LOGGER.info("Will turn thermo off")
+            _LOGGER.debug("Will turn thermo off")
             self._is_on = False
         self.sendUpdateToApi()
 
     @property
     def available(self) -> bool:
         """Return if thermostat is available."""
-#        _LOGGER.info('Called available')
-        return self.current_operation is not None
+        """_LOGGER.debug('Called available')"""
+        return True
 
     @property
-    def operation_list(self):
+    def hvac_modes(self):
         """Return the list of available operation modes."""
-#        _LOGGER.info('Called operation_list')
-        return [STATE_HEAT,STATE_OFF, STATE_IDLE]
+        """_LOGGER.debug('Called operation_list')"""
+        return [HVAC_MODE_HEAT, HVAC_MODE_OFF, HVAC_MODE_AUTO]
 
     @property
-    def is_on(self):
-        """Return true if the device is on."""
-#        _LOGGER.info('Called is_on')
-        return self._is_on
+    def hvac_mode(self):
+        """Return hvac operation ie. heat, cool mode."""
+        """_LOGGER.debug('Called is_on')"""
+        if self._is_on:
+            return HVAC_MODE_HEAT
+        return HVAC_MODE_OFF
+
+#    @property
+#    def current_operation(self):
+#        """Return current operation ie. heat, cool, idle."""
+#        _LOGGER.debug('requested current_operation: %s',self._current_operation)
+#        return self._current_operation
+
+    @property
+    def hvac_action(self):
+        """Return the current running hvac operation if supported.
+        Need to be one of CURRENT_HVAC_*.
+        """
+        if self._is_on:
+            if self._burner_on:
+                return CURRENT_HVAC_HEAT
+            else:
+                return CURRENT_HVAC_IDLE
+        else:
+            return CURRENT_HVAC_OFF
 
     def sendUpdateToApi(self):
         if not self.isValidToken:
             self.updateToken()
             if not self.isValidToken:
+                _LOGGER.error('Token failed to update. Sorry')
                 return
 
         _headers = {
@@ -277,44 +307,52 @@ class OlympiaElectronicsThermostat(ClimateDevice):
             'turn_on'     : ton,
             'setpoint'    : "{}".format(self._target_temperature)
         }
-
+        _LOGGER.debug('Will send new data to the api %s',_post_data)
         r = requests.put("https://iot-api.olympia-electronics.gr/v1/thermostats/{}/settings/".format(self._id),
                       headers=_headers, data=_post_data)
+        _LOGGER.debug('Got reply %s',r.text)
         self.update()
 
     def set_temperature(self, **kwargs):
         """Set new target temperatures."""
-#        _LOGGER.info('Called set_temperature')
+        """_LOGGER.debug('Called set_temperature')"""
         if kwargs.get(ATTR_TEMPERATURE) is not None:
             self._target_temperature = kwargs.get(ATTR_TEMPERATURE)
+        self.schedule_update_ha_state()
         self.sendUpdateToApi()
 
-    def turn_on(self):
+    def set_operation_mode(self, operation_mode):
+        """Set new operation mode."""
+        _LOGGER.debug('setting current_operation: %s',operation_mode)
+        self._current_operation = operation_mode
+        self.schedule_update_ha_state()
+
+    def set_hvac_mode(self, hvac_mode):
         """Turn on."""
-#        _LOGGER.info('Called turn_on')
-        self._is_on = True
-        self.sendUpdateToApi()
-
-    def turn_off(self):
-        """Turn off."""
-#        _LOGGER.info('Called turn_off')
-        self._is_on = False
+        if hvac_mode == HVAC_MODE_HEAT:
+            self._is_on = True
+        elif hvac_mode == HVAC_MODE_OFF:
+            self._is_on = False
+        else:
+            _LOGGER.error("Unrecognized hvac mode: %s", hvac_mode)
+            return
+        self.schedule_update_ha_state()
         self.sendUpdateToApi()
 
     @property
     def min_temp(self):
         """Return the minimum temperature."""
-#        _LOGGER.info('Called min_temp')
+        """_LOGGER.debug('Called min_temp')"""
         return self._min_temp
 
     @property
     def max_temp(self):
         """Return the maximum temperature."""
-#        _LOGGER.info('Called max_temp')
+        """_LOGGER.debug('Called max_temp')"""
         return self._max_temp
 
     @property
     def precision(self):
         """Return the precision."""
-#        _LOGGER.info('Called precision')
+        """_LOGGER.debug('Called precision')"""
         return self._precision
